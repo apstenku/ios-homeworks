f083ebf00dfe2d55c30fa99475185bbcb6355511
Maria Prokofeva <mariya_n1@inbox.ru>
2022-04-08 16:06:19 +0300

    Добавлены IBOoutlet'ы, Profile.storyboard,ProfileViewController

--- Netology_IB_Instruments/AppDelegate.swift
+++ Netology_IB_Instruments/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Netology_IB_Instruments
 //
-//  Created by Мария Прокофьева on 08.04.2022.
+//  Created by Мария Прокофьева
 //
 
 import UIKit
---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Мария Прокофьева
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var dateBirthdayLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+}
